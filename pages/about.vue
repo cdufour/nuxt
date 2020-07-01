5252d521904b9dd49c114a9a81943be47d7ee71c
cdufour <opusidea@gmail.com>
2020-07-01 16:13:40 +0200

    nuxt start

---
+++ pages/about.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <h1>A propos</h1>
+        <p>{{ counter }}</p>
+        <button @click="incr">Increment</button>
+        <p v-if="isEnough">BASTA !</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                counter: 0
+            }
+        },
+        methods: {
+            incr() {
+                this.counter++
+            }
+        },
+        computed: {
+            isEnough() {
+                return this.counter > 10
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
